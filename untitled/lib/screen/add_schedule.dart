76c5c2d6160cd84a481683f20a6ceb4b881de10d
eyeofsol <sjh991101@gmail.com>
2024-11-18 15:55:51 +0900

    플로팅 버튼 추가 및 연결
    로그인 버튼 클릭 시 달력 화면으로 이동, 달력 화면의 플로팅 버튼 클릭 시 일정 추가 화면으로 이동

--- untitled/lib/screen/add_schedule.dart
+++ untitled/lib/screen/add_schedule.dart
@@ -2,9 +2,7 @@
 import 'package:flutter/material.dart';
 import '../widgets/custom_button.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class AddSchedule extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- untitled/lib/screen/login_screen.dart
+++ untitled/lib/screen/login_screen.dart
@@ -1,5 +1,6 @@
 // screens/login_screen.dart
 import 'package:flutter/material.dart';
+import 'package:untitled/screen/home_screen.dart';
 import '../widgets/custom_button.dart'; // 커스텀 버튼을 사용하기 위해 불러옵니다.
 
 class LoginScreen extends StatelessWidget {
@@ -50,7 +51,10 @@ class LoginScreen extends StatelessWidget {
                 text: "Login", // 버튼 텍스트
                 color: const Color(0xFF1976D2), // 버튼 배경색
                 onPressed: () {
-                  // 로그인 기능 추가
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const HomeScreen())
+                  );
                 },
               ),
               const SizedBox(height: 10),
